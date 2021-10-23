0b0e890955e2839d329ab596a3a7d7e730dd848d
PRASHANTH-KODIMALA <91456899+PRASHANTH-KODIMALA@users.noreply.github.com>
2021-10-23 23:45:31 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+//program to check if the given number is odd or even
+import java.util.*;
+public class OddEven{
+	static void check(){
+		System.out.print("Enter a Number: "); 
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num%2==0){
+			System.out.println(num+" is Even Number ");
+		}
+		else{
+			System.out.println(num+" is Odd Number ");
+		}
+	}
+	public static void main(String args[]){
+		check();
+	}
+}
